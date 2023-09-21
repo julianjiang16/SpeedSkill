21b65b7c7eed35cdbc316c0765874493c58bfd13
julianjiang <julianjiang@tencent.com>
2023-09-21 16:19:13 +0800

    初步想法

---
+++ src/main/java/com/tengyu/skill/Constants.java
@@ -0,0 +1,6 @@
+package com.tengyu.skill;
+
+public class Constants {
+
+    public static final String SYSTEM_NODE_ROOT_PATH = "/speedSkill/clusters/";
+}
---
+++ src/main/java/com/tengyu/skill/zk/ZkService.java
@@ -0,0 +1,12 @@
+package com.tengyu.skill.zk;
+
+public class ZkService {
+
+    // 系统启动，可以根据配置开关在指定目录下创建zk临时节点，并对指定目录添加监听, 动态更新集群信息  /speedSkill/clusters/...
+
+    // /speedSkill/clusters/机器ip/商品1 - 库存  /speedSkill/clusters/机器ip/商品n - 库存  ...
+
+
+    //
+
+}
